a54019b0b28706185853ba91f8962548e7f97181
truongan-8827 <phanletruongan4@gmail.com>
2026-06-26 11:48:35 +0700

    adfaadfadf

--- OOP/src/Buoi2/PhanSo.java
+++ OOP/src/Buoi2/PhanSo.java
@@ -15,6 +15,7 @@ public class PhanSo {
 			if(mau!=0) break;
 			else System.out.println("Vui long nhap lai!");
 		}
+		this.chuanhoaPhanSo();
 	}
 	public void hienThi() {
 		if(tu==0) System.out.println(0);
@@ -27,13 +28,13 @@ public class PhanSo {
 		else if(mau==1) return "tu"; 
 		else return tu+"/"+mau;
 	}
-	public void chuanhoaPhanSo() { //*******
-		int t;
-		if(tu>mau) t=mau;
-		else t=tu;
-		for(int i=1;i<=t;i++) {
-			if(tu%i==0 && mau%i==0) {tu/=i; mau/=i;}
+	public void chuanhoaPhanSo() {
+		//UCLN
+		int a=tu,b=mau,r;
+		while(b!=0) {
+			r=a%b; a=b; b=r;
 		}
+		tu/=a; mau/=a; //phan so da chuan hoa
 	}
 	public void nghichDao() {int temp=tu; tu=mau; mau=temp;}
 	public PhanSo giaTriNghichDao() {return new PhanSo(mau,tu);}
@@ -45,37 +46,80 @@ public class PhanSo {
 		else return false;
 	}
 	public PhanSo cong(PhanSo a) {
-		int tu1=tu*a.mau+a.tu*mau;
-		int mau1=mau*a.mau;
-		return new PhanSo(tu1,mau1);
+		PhanSo PS = new PhanSo(tu*a.mau+a.tu*mau,mau*a.mau);
+		PS.chuanhoaPhanSo();
+		return PS;
 	}
-	public PhanSo cong(int n) {return new PhanSo(tu+n*mau,mau);}
+	public PhanSo tru(PhanSo a) {
+		PhanSo PS = new PhanSo(tu*a.mau-a.tu*mau,mau*a.mau);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
+	public PhanSo nhan(PhanSo a) {
+		PhanSo PS = new PhanSo(tu*a.tu,mau*a.mau);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
+	public PhanSo chia(PhanSo a) {
+		PhanSo PS = new PhanSo(tu*a.mau,mau*a.tu);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
+	
+	public PhanSo cong(int n) {
+		PhanSo PS = new PhanSo(tu+n*mau,mau);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
+	public PhanSo tru(int n) {
+		PhanSo PS = new PhanSo(tu-n*mau,mau);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
+	public PhanSo nhan(int n) {
+		PhanSo PS = new PhanSo(tu*n,mau);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
+	public PhanSo chia(int n) {
+		PhanSo PS = new PhanSo(tu,mau*n);
+		PS.chuanhoaPhanSo();
+		return PS;
+	}
 	
 	public PhanSo[] taoDS() {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("nhap so luong phan tu DS");
 		int n; n=sc.nextInt();
 		PhanSo[] ds=new PhanSo[n];
-		//cap phat bo nho cho danh sach
 		for(int i=0;i<n;i++) ds[i] = new PhanSo();
 		for(int i=0;i<n;i++) {
 			System.out.println("nhap phan so thu "+(i+1));
 			ds[i].nhapPhanSo();
+			ds[i].chuanhoaPhanSo();
 		}
 		return ds;
 	}
 	public void hienthiDS(PhanSo[] d) {
 		for(int i=0;i<d.length;i++) { System.out.println("phan so thu "+(i+1)); d[i].hienThi();}
 	}
-	public double tongDS(PhanSo[] d) {
-		double sum=0.0;
-		for(int i=0;i<d.length;i++) sum+=d[i].tinhPhanSo();
-		return sum;
+	public PhanSo tongDS(PhanSo[] d) {
+		PhanSo PS = new PhanSo(0,1);
+		for(int i=0;i<d.length;i++) {
+			PS=PS.cong(d[i]);
+		}
+		return PS;
 	}
-	public double maxDS(PhanSo[] d) {
+	public PhanSo maxDS(PhanSo[] d) {
 		double max=d[0].tinhPhanSo();
-		for(int i=0;i<d.length;i++) {if(max<d[i].tinhPhanSo()) max=d[i].tinhPhanSo();}
-		return max;
+		int count=0;
+		for(int i=0;i<d.length;i++) {
+			if(max<d[i].tinhPhanSo()) {
+				max=d[i].tinhPhanSo();
+				count++;
+			}
+		}
+		return new PhanSo(d[count]);
 	}
 	public void sapXepTang(PhanSo[] d) {
 		for(int i=0;i<d.length;i++)
--- OOP/src/Buoi2/SDPhanSo.java
+++ OOP/src/Buoi2/SDPhanSo.java
@@ -4,16 +4,14 @@ public class SDPhanSo {
 
 	public static void main(String[] args) {
 		System.out.println("Tao phan so a");
-		PhanSo a=new PhanSo(3,7); a.hienThi();
+		PhanSo a=new PhanSo(3,7); a.chuanhoaPhanSo(); a.hienThi();
 		System.out.println("Tao phan so b");
-		PhanSo b=new PhanSo(4,9); b.hienThi();
+		PhanSo b=new PhanSo(4,9); b.chuanhoaPhanSo(); b.hienThi();
 		PhanSo x=new PhanSo(); PhanSo y = new PhanSo();
 		System.out.println("Nhap phan so x");
 		x.nhapPhanSo();  x.hienThi(); 
 		System.out.println("Nhap phan so y");
 		y.nhapPhanSo(); y.hienThi();
-		x.chuanhoaPhanSo(); y.chuanhoaPhanSo();
-		System.out.println("x va y sau khi chuan hoa la"); x.hienThi(); y.hienThi();
 		System.out.println("Gia tri x sau khi nghich dao la");
 		PhanSo A=x.giaTriNghichDao(); A.hienThi();
 		System.out.println("gia tri x+y la"); 
